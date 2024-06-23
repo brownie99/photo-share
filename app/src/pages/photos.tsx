9fb04b62b8bb411f575bde220744ada9f4737e88
brownie99 <scurry99@hotmail.co.uk>
2024-06-23 23:15:13 +0100

    Add photo upload
    Photo upload added using axios post with form data.
    
    Also implemented login with barer token for auth.

--- app/src/pages/photos.tsx
+++ app/src/pages/photos.tsx
@@ -1,3 +1,90 @@
+import React, { Dispatch, useEffect, useState, SetStateAction } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import axios from "axios";
+
+async function login(pass: string) {
+  const res = await fetch("http://192.168.1.44:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pass }),
+  }).then((res) => res.json());
+  if (res) {
+    if (res.error) {
+      console.log(res.error);
+      return null;
+    }
+    return res.token;
+  }
+  return null;
+}
+
 export default function Photos() {
-  return <h1>Photos</h1>;
+  const { register, handleSubmit } = useForm();
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("authToken") !== null &&
+      localStorage.getItem("authToken") !== "",
+  );
+  console.log(loggedIn);
+  async function onSubmit(data: FieldValues) {
+    const formData = new FormData();
+    // if (data.file.length === 0) {
+    //   return;
+    // }
+    for (const file of data.file) {
+      formData.append("file", file);
+    }
+
+    const res = await axios
+      .post("http://192.168.1.44:3000/upload", formData, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((error) => {
+        console.log("caught error");
+        if (error.response.status === 401) {
+          console.log("unauthorized, logging in");
+          localStorage.setItem("authToken", "");
+          setLoggedIn(false);
+          console.log(loggedIn);
+          return undefined;
+        } else console.log(error);
+      });
+    if (res !== undefined) alert(JSON.stringify(`${res}`));
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    let token = localStorage.getItem("authToken");
+    console.log(token);
+    async function tryLogin() {
+      while (token === null || token === "") {
+        const pass = window.prompt("Please enter password");
+        if (pass === null) {
+          continue;
+        }
+        token = await login(pass);
+      }
+
+      localStorage.setItem("authToken", token);
+      setLoggedIn(true);
+    }
+    if (token === null || token === "") tryLogin();
+  }, [loggedIn]);
+
+  return (
+    <>
+      <h1>Photos</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="file" {...register("file")} multiple />
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,5 +1,77 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+
+const PASSWORD = "love";
+const secret = "photosharebackendsecretkey123";
+
+const port = 3000;
+const app = express();
+app.use(cors());
+
+function authenticateToken(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log(`token: ${token}`);
+
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, secret, (err: any) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(401);
+
+    next();
+  });
+}
+
 function start() {
   console.log("started");
 }
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  console.log("POST /login");
+  if (req.body === undefined || req.body.pass === undefined) {
+    return res.status(401).json({ error: "Incorrect password" });
+  }
+  if (req.body.pass !== PASSWORD) {
+    return res.status(401).json({ error: "Incorrect password" });
+  }
+
+  jwt.sign({ id: Date.now() }, secret, { expiresIn: "30s" }, (err, token) => {
+    res.json({ token });
+  });
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", authenticateToken, upload.array("file"), (req, res) => {
+  console.log(req);
+  console.log(req.files);
+  console.log("POST /upload");
+  if (!req.files) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  res.json({
+    message: "Files uploaded successfully",
+  });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 start();
