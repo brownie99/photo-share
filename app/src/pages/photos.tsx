a1ab9052e234fd082acacaa818107f92d3fa921a
brownie99 <scurry99@hotmail.co.uk>
2024-07-13 17:12:30 +0100

    Login mostly working
    Login now working, no retry facility yet.
    
    Removed strict mode (causing problems CBA to fix).
    
    Installed react-photo-gallery for future use.

--- app/src/pages/photos.tsx
+++ app/src/pages/photos.tsx
@@ -1,10 +1,20 @@
-import React, { Dispatch, useEffect, useState, SetStateAction } from "react";
+import React, {
+  Dispatch,
+  useEffect,
+  useState,
+  SetStateAction,
+  useCallback,
+} from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import axios from "axios";
 import ProgressBar from "components/common/ProgressBar";
 import styled from "styled-components";
 
-async function login(pass: string) {
+async function login(
+  pass: string,
+  setLoggingIn: React.Dispatch<React.SetStateAction<boolean>>,
+) {
+  setLoggingIn(() => true);
   const res = await fetch("http://192.168.1.44:3000/login", {
     method: "POST",
     headers: {
@@ -12,18 +22,29 @@ async function login(pass: string) {
     },
     body: JSON.stringify({ pass }),
   }).then((res) => res.json());
+  setLoggingIn(() => false);
   if (res) {
     if (res.error) {
-      console.log(res.error);
+      console.log(`login error: ${res.error}`);
       return null;
     }
+    console.log(`login res toekn: ${res.token}`);
     return res.token;
   }
+  console.log("no response");
   return null;
+}
+
+async function getPhotos() {
+  const token = sessionStorage.getItem("authToken");
+  if (token === null || token === "") {
+    return;
+  }
 }
 
 const ProgressContainer = styled.div`
   width: 50%;
+  margin: auto;
   /* left: 50%; */
   /* transform: translate(-50%, 0); */
 `;
@@ -31,66 +52,76 @@ const ProgressContainer = styled.div`
 export default function Photos() {
   const { register, handleSubmit } = useForm();
   const [loggedIn, setLoggedIn] = useState(
-    localStorage.getItem("authToken") !== null &&
-      localStorage.getItem("authToken") !== "",
+    sessionStorage.getItem("authToken") !== null &&
+      sessionStorage.getItem("authToken") !== "",
   );
+  const [loggingIn, setLoggingIn] = useState(false);
   const [progress, setProgress] = useState<number | undefined>(undefined);
-  console.log(loggedIn);
-  async function onSubmit(data: FieldValues) {
-    const formData = new FormData();
-    // if (data.file.length === 0) {
-    //   return;
-    // }
-    for (const file of data.file) {
-      formData.append("file", file);
-    }
-
-    const res = await axios
-      .post("http://192.168.1.44:3000/upload", formData, {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("authToken"),
-        },
-        onUploadProgress: (progressEvent) => {
-          const { loaded, total } = progressEvent;
-          total !== undefined
-            ? setProgress(Math.floor((loaded * 100) / total))
-            : setProgress(undefined);
-        },
-      })
-      .then((res) => {
-        return res;
-      })
-      .catch((error) => {
-        console.log("caught error");
-        if (error.response.status === 401) {
-          console.log("unauthorized, logging in");
-          localStorage.setItem("authToken", "");
-          setLoggedIn(false);
-          console.log(loggedIn);
-          return undefined;
-        } else console.log(error);
-      });
-    if (res !== undefined) alert(JSON.stringify(`${res.data.message}`));
-  }
-
-  useEffect(() => {
-    console.log("useEffect");
-    let token = localStorage.getItem("authToken");
-    console.log(token);
-    async function tryLogin() {
-      while (token === null || token === "") {
-        const pass = window.prompt("Please enter password");
-        if (pass === null) {
-          continue;
-        }
-        token = await login(pass);
+  console.log(`loggedIn: ${loggedIn}`);
+  const onSubmit = useCallback(
+    async function (data: FieldValues) {
+      const formData = new FormData();
+      // if (data.file.length === 0) {
+      //   return;
+      // }
+      for (const file of data.file) {
+        formData.append("file", file);
       }
 
-      localStorage.setItem("authToken", token);
-      setLoggedIn(true);
+      const res = await axios
+        .post("http://192.168.1.44:3000/upload", formData, {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("authToken"),
+          },
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            total !== undefined
+              ? setProgress(Math.floor((loaded * 100) / total))
+              : setProgress(undefined);
+          },
+        })
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.log("caught error");
+          if (error.response.status === 401) {
+            console.log("unauthorized, logging in");
+            sessionStorage.setItem("authToken", "");
+            setLoggedIn(false);
+            console.log(`loggedIn: ${loggedIn}`);
+            return undefined;
+          } else console.log(error);
+        });
+      if (res !== undefined) alert(JSON.stringify(`${res.data.message}`));
+    },
+    [loggedIn, setProgress],
+  );
+
+  useEffect(() => {
+    if (loggedIn === true || loggingIn === true) {
+      return;
     }
-    if (token === null || token === "") tryLogin();
-  }, [loggedIn]);
+
+    async function tryLogin() {
+      const pass = window.prompt("Enter password");
+      if (pass === null) {
+        throw new Error("no password provided");
+      }
+
+      token = await login(pass, setLoggingIn);
+
+      if (token === null) {
+        throw new Error("token undefined");
+      }
+
+      sessionStorage.setItem("authToken", token);
+      setLoggedIn(() => true);
+    }
+    let token = sessionStorage.getItem("authToken");
+
+    tryLogin();
+  }, [loggedIn, loggingIn]);
 
   return (
     <>
