c7c762503c8bc9030257310dad2aa325855e8dd9
brownie99 <scurry99@hotmail.co.uk>
2025-03-03 23:10:47 +0000

    Photo retrieval and auth
    Added get to photo backend and auth utilities.

--- app/src/pages/photos.tsx
+++ app/src/pages/photos.tsx
@@ -1,14 +1,11 @@
-import React, {
-  Dispatch,
-  useEffect,
-  useState,
-  SetStateAction,
-  useCallback,
-} from "react";
+import React, { Dispatch, useEffect, useState, useCallback } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import axios from "axios";
 import ProgressBar from "components/common/ProgressBar";
 import styled from "styled-components";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import DeepEqual from "deep-equal";
 
 async function login(
   pass: string,
@@ -35,11 +32,101 @@ async function login(
   return null;
 }
 
-async function getPhotos() {
-  const token = sessionStorage.getItem("authToken");
-  if (token === null || token === "") {
+async function getPhotos(
+  loggedIn: boolean,
+  setLoggedIn: Dispatch<React.SetStateAction<boolean>>,
+) {
+  if (!loggedIn) {
     return;
   }
+
+  const res = await fetch("http://192.168.1.44:3000/photos", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + sessionStorage.getItem("authToken"),
+    },
+  })
+    .then((res) => {
+      if (res.status === 401) {
+        throw res;
+      }
+
+      return res.json();
+    })
+    .catch((error) => {
+      console.log("caught error");
+      console.log(error);
+      if (error.status === 401) {
+        console.log("unauthorized, logging in");
+        sessionStorage.setItem("authToken", "");
+        setLoggedIn(false);
+        console.log(`loggedIn: ${loggedIn}`);
+        return undefined;
+      } else console.log(error);
+    });
+  if (res) {
+    if (res.error) {
+      console.log(res.error);
+    }
+    console.log(res);
+    return res;
+  }
+  console.log("no response");
+  return null;
+}
+
+async function getPhoto(
+  loggedIn: boolean,
+  setLoggedIn: Dispatch<React.SetStateAction<boolean>>,
+  photo: string,
+) {
+  if (!loggedIn) {
+    return;
+  }
+
+  const res = await fetch("http://192.168.1.44:3000/photos", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + sessionStorage.getItem("authToken"),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ photo }),
+  })
+    .then((res) => {
+      if (res.status === 401) {
+        throw res;
+      }
+
+      return res.blob();
+    })
+    .catch((error) => {
+      console.log("caught error");
+      if (error.status === 401) {
+        console.log("unauthorized, logging in");
+        sessionStorage.setItem("authToken", "");
+        setLoggedIn(false);
+        console.log(`loggedIn: ${loggedIn}`);
+        return undefined;
+      } else console.log(error);
+    });
+  if (res) {
+    return await res;
+
+    // let imgData = "";
+
+    // while (true) {
+    //   const { done, value } = await res.read();
+    //   if (done) {
+    //     // Do something with last chunk of data then exit reader
+    //     imgData += value;
+    //     return imgData;
+    //   }
+    //   // Otherwise do something here to process current chunk
+    //   imgData += value;
+    // }
+  }
+  console.log("no response");
+  return null;
 }
 
 const ProgressContainer = styled.div`
@@ -57,13 +144,19 @@ export default function Photos() {
   );
   const [loggingIn, setLoggingIn] = useState(false);
   const [progress, setProgress] = useState<number | undefined>(undefined);
+  const [photos, setPhotos] = useState<Array<string>>([]);
+  const [photoList, setPhotoList] = useState<Array<string>>([]);
+  const [galleryItems, setGalleryItems] = useState<
+    Array<ReactImageGalleryItem>
+  >([]);
+
   console.log(`loggedIn: ${loggedIn}`);
   const onSubmit = useCallback(
     async function (data: FieldValues) {
       const formData = new FormData();
-      // if (data.file.length === 0) {
-      //   return;
-      // }
+      if (data.file.length === 0) {
+        return;
+      }
       for (const file of data.file) {
         formData.append("file", file);
       }
@@ -123,6 +216,78 @@ export default function Photos() {
     tryLogin();
   }, [loggedIn, loggingIn]);
 
+  const getMorePhotos = useCallback(
+    async function () {
+      const gotPhotos = await getPhotos(loggedIn, setLoggedIn);
+      if (
+        gotPhotos === null ||
+        gotPhotos === undefined ||
+        gotPhotos.length === 0
+      ) {
+        return;
+      }
+
+      console.log("got");
+      console.log(gotPhotos);
+      console.log("current");
+      console.log(photoList);
+
+      if (DeepEqual(gotPhotos, photoList)) {
+        console.log("deep equal");
+        return;
+      }
+
+      console.log("got");
+      console.log(gotPhotos);
+      console.log("current");
+      console.log(photoList);
+
+      setPhotoList(gotPhotos);
+    },
+    [loggedIn, photoList],
+  );
+
+  useEffect(() => {
+    if (photoList.length === 0 || photoList.length === photos.length) {
+      console.log("returning");
+      return;
+    }
+
+    const tmpPhotos = Array.from(photos);
+
+    Object.keys(photoList).forEach(async (key) => {
+      const gotPhoto = await getPhoto(loggedIn, setLoggedIn, photoList[key]);
+      console.log(gotPhoto);
+      if (gotPhoto !== null && gotPhoto !== undefined) {
+        tmpPhotos.push(window.URL.createObjectURL(gotPhoto));
+      }
+    });
+    setPhotos(() => tmpPhotos);
+  }, [loggedIn, photos, photoList]);
+
+  useEffect(() => {
+    console.log("getting photos");
+
+    getMorePhotos();
+  }, [loggedIn, photos, getMorePhotos]);
+
+  // console.log("current");
+  // console.log(btoa(photo));
+
+  console.log("photos");
+  console.log(photos);
+  useEffect(() => {
+    console.log("updating gallery items");
+    setGalleryItems(
+      photos.map((photo) => {
+        return { original: photo, thumbnail: photo };
+      }),
+    );
+  }, [photos]);
+
+  console.log("gallery items");
+  console.log(galleryItems);
+
   return (
     <>
       <h1>Photos</h1>
@@ -136,6 +301,7 @@ export default function Photos() {
           <ProgressBar progress={progress} bgColor="#8742f5" />
         </ProgressContainer>
       ) : null}
+      <ImageGallery items={galleryItems} />
     </>
   );
 }
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,9 +2,13 @@ import express from "express";
 import multer from "multer";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import { readdir, readFile, stat } from "fs/promises";
+import { statSync } from "fs";
 
 const PASSWORD = "love";
 const secret = "photosharebackendsecretkey123";
+const photos: Map<number, string> = new Map();
+const photoDir = "H:\\PhotoShare\\photo-share\\back-end\\uploads\\";
 
 const port = 3000;
 const app = express();
@@ -28,13 +32,30 @@ function authenticateToken(req, res, next) {
   });
 }
 
-function start() {
+async function updatePhotos() {
+  console.log("updating photos");
+  (await readdir(photoDir))
+    .sort(function (a, b) {
+      return (
+        statSync(photoDir + a).mtime.getTime() -
+        statSync(photoDir + b).mtime.getTime()
+      );
+    })
+    .forEach((file) => {
+      photos.set(photos.size, file);
+    });
+  console.log("photos updated");
+}
+
+async function start() {
+  console.log("starting");
+  await updatePhotos();
   console.log("started");
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, photoDir);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -67,11 +88,31 @@ app.post("/upload", authenticateToken, upload.array("file"), (req, res) => {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
+  updatePhotos();
+
   res.json({
     message: "Files uploaded successfully",
   });
 });
 
+app.post("/photos", authenticateToken, async (req, res) => {
+  console.log("GET /photos");
+  console.log(req);
+  if (!req.body.photo) {
+    console.log("sending all photos");
+    console.log({ photos });
+    res.json(Object.fromEntries(photos));
+    return;
+  }
+  try {
+    await stat(photoDir + req.body.photo);
+  } catch {
+    res.sendStatus(404);
+  }
+
+  res.sendFile(photoDir + req.body.photo);
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
-start();
+await start();
